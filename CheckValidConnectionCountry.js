657d813716380acb7f47eb29b992332e356fe0eb
jofiisi <jofiisi.dkt@gmail.com>
2023-04-22 16:06:20 +0200

    the ability to enter a country and list the neighbors

--- CheckValidConnectionCountry.js
+++ CheckValidConnectionCountry.js
@@ -9,19 +9,17 @@ async function getCsvFile() {
   return data;
 }
 
-var inputCountries = [["Austria"]["Belgium"]];
-
 function getCountryCountryNeighbor() {
   return new Promise(function (resolve, reject) {
 
     getCsvFile().then(function (data) {
-      csvData = data;
+      let csvData = data;
       //Convert to 2d array with only name & border name 
-      var csvData = data.split('\n');
-      var countryCountryNeighbor = [];
-      for (var i = 1; i < csvData.length; i++) {
+      csvData = data.split('\n');
+      let countryCountryNeighbor = [];
+      for (let i = 1; i < csvData.length; i++) {
         csvData[i] = csvData[i].replace(/"/g, '');
-        var parts = csvData[i].split(',');
+        let parts = csvData[i].split(',');
         countryCountryNeighbor.push([parts[1], parts[3]]);
       }
       csvData = undefined;
@@ -33,23 +31,60 @@ function getCountryCountryNeighbor() {
 }
 
 function getNeighbors(inputCountry, countryCountryNeighbor, outputCountries) {
-  var i = 0;
-  while (countryCountryNeighbor[i][0] != inputCountry) {
-    i++;
+  let i = 0;
+  try {
+    while (countryCountryNeighbor[i][0] != inputCountry) {
+      i++;
+    }
+  } catch (error) {
+    return 1;
   }
-  while(countryCountryNeighbor[i][0] == inputCountry)
-  {
+  while (countryCountryNeighbor[i][0] == inputCountry) {
     outputCountries.push(countryCountryNeighbor[i][1]);
     i++;
-  } 
+  }
+  return outputCountries;
 }
 
+document.addEventListener("DOMContentLoaded", function () {
 
+  async function main() {
+    let countryCountryNeighbor;
+    const inputCountry = document.getElementById("inputCountry");
+    const outputList = document.getElementById("outputList");
+    var outputCountries = [];
+    countryCountryNeighbor = await getCountryCountryNeighbor();
+    
+    inputCountry.addEventListener("keydown", function (event) {
+      if (event.key === 'Enter') {
+        outputCountries = [];
+        outputCountries = getNeighbors(inputCountry.value, countryCountryNeighbor, outputCountries);
+        if (outputCountries != 1) {
+          document.getElementById("wrongInput").innerHTML = "";
+          while (outputList.firstChild) {
+            outputList.removeChild(outputList.firstChild);
+          }
+          for (let i = 0; i < outputCountries.length; i++) {
+            let liElement = document.createElement('li');
+            liElement.innerHTML = outputCountries[i];
+            outputList.appendChild(liElement);
+          }
+        } else {
+          document.getElementById("wrongInput").innerHTML = "Please Enter a valid Country";
+        }
+      }
+    });
+  }
+
+  main();
+});
+
+/* NAJA alt & irrelevant
 getCountryCountryNeighbor().then(function(countryCountryNeighbor) {
   var Neighbors = [];
   getNeighbors("Austria", countryCountryNeighbor, Neighbors);
   console.table(Neighbors);
 }).catch(function(error) {
   console.error(error);
-});
+});*/
 
